2c9acf65ee7347f636c78ac4a2c19369030908d5
Anas <anas.chaabane98@gmail.com>
2023-03-04 23:37:49 +0100

    refactor using funtion based suggsestions element

--- src/components/SeachBar/SearchBar.tsx
+++ src/components/SeachBar/SearchBar.tsx
@@ -10,14 +10,70 @@ interface Props {
 
 const SearchBar = ({ data, placeHolder }: Props) => {
   const [searchQuery, setSeachQuery] = useState<string>('');
+  const [showSuggestions, setshowSuggestions] = useState<Boolean>(false);
+  const [activeIndex, setActiveindex] = useState<number>(0);
 
-  const handleSearchQuery = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setSeachQuery(e.target.value);
-  };
-
-  const filterSearch = (): string[] => {
+  function filterSearch(): string[] {
     return data.filter((elem) =>
       elem.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }
+
+  function handleSearchQueryOrKeyDown(e: any): void {
+    console.log(e.type);
+    
+    if (e.type === 'change') {
+      console.log('this is change');
+      setSeachQuery(e.target.value);
+      setshowSuggestions(true);
+    }
+
+    if (e.type === 'keydown') {
+      const { code } = e;
+      console.log(code);
+      e.preventDefault();
+
+      if (code === 'Tab' && showSuggestions && filterSearch().length > 0) {
+        setSeachQuery(filterSearch()[activeIndex]);
+        setshowSuggestions(false);
+      }
+
+      if (code === 'ArrowUp' && activeIndex > 0) {
+        setActiveindex(activeIndex + 1);
+        console.log('this is ArrowUp');
+      }
+
+      if (code === 'ArrowDown' && activeIndex < filterSearch().length) {
+        setActiveindex(activeIndex - 1);
+        console.log('this is ArrowDown');
+      }
+    }
+  }
+
+  function handelSuggsestionClick(value: string): void {
+    setSeachQuery(value);
+    setshowSuggestions(false);
+  }
+
+  function handleSumit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+  }
+
+  const renderSuggestions = () => {
+    if (!showSuggestions) return null;
+
+    return (
+      <ul>
+        {filterSearch().map((elem, index) => (
+          <li
+            key={elem}
+            onClick={() => handelSuggsestionClick(elem)}
+            className={index === activeIndex ? styles.active : ''}
+          >
+            {elem}
+          </li>
+        ))}
+      </ul>
     );
   };
 
@@ -26,21 +82,13 @@ const SearchBar = ({ data, placeHolder }: Props) => {
       <div>
         <TextField
           value={searchQuery}
-          onChange={handleSearchQuery}
+          onChange={handleSearchQueryOrKeyDown}
+          onKeyDown={handleSearchQueryOrKeyDown}
           id='outlined-basic'
           label={placeHolder}
           variant='outlined'
         />
-
-        {searchQuery.length > 0 && (
-          <ul className='sugsession'>
-            {filterSearch().map((item) => (
-              <li key={item}>
-                <a href={item}>{item}</a>
-              </li>
-            ))}
-          </ul>
-        )}
+        {renderSuggestions()}
       </div>
       <Button color='secondary' variant='contained'>
         Search
