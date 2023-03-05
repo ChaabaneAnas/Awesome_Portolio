01d78c695d9cfae3d61d9f3a63c1b3c738ef841f
Anas <anas.chaabane98@gmail.com>
2023-03-05 23:36:18 +0100

    submit & navigation

--- src/components/SeachBar/SearchBar.tsx
+++ src/components/SeachBar/SearchBar.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button, TextField } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import styles from './SearchBar.module.css';
-import { bgcolor } from '@mui/system';
 
 interface Props {
   data: string[];
@@ -12,6 +12,7 @@ const SearchBar = ({ data, placeHolder }: Props) => {
   const [searchQuery, setSeachQuery] = useState<string>('');
   const [showSuggestions, setshowSuggestions] = useState<Boolean>(false);
   const [activeIndex, setActiveindex] = useState<number>(0);
+  const navigate = useNavigate();
 
   console.log('render');
   function filterSearch(): string[] {
@@ -63,6 +64,7 @@ const SearchBar = ({ data, placeHolder }: Props) => {
 
   function handleSumit(e: React.FormEvent<HTMLFormElement>): void {
     e.preventDefault();
+    navigate(`/#${searchQuery}`);
   }
 
   const renderSuggestions = () => {
@@ -74,7 +76,7 @@ const SearchBar = ({ data, placeHolder }: Props) => {
             onClick={() => handelSuggsestionClick(elem)}
             className={index === activeIndex ? styles.active : ''}
           >
-            {elem}
+            <a href=''>{elem}</a>
           </li>
         ))}
       </ul>
@@ -82,7 +84,7 @@ const SearchBar = ({ data, placeHolder }: Props) => {
   };
 
   return (
-    <form className={`${styles.flex} searchContiner`}>
+    <form onSubmit={handleSumit} className={`${styles.flex} searchContiner`}>
       <div className={styles.inputs}>
         <TextField
           value={searchQuery}
@@ -94,7 +96,7 @@ const SearchBar = ({ data, placeHolder }: Props) => {
         />
         {showSuggestions && searchQuery.length > 0 && renderSuggestions()}
       </div>
-      <Button color='secondary' variant='contained'>
+      <Button type='submit ' color='secondary' variant='contained'>
         Search
       </Button>
     </form>
